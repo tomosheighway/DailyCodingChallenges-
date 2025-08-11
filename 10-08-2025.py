cafcc5b8eaedc9291c2444fa46f29f4e33ce2091
Tomos Heighway <40778852+tomosheighway@users.noreply.github.com>
2025-08-11 18:31:31 +0100

    Palindrome checker

---
+++ 10-08-2025.py
@@ -0,0 +1,20 @@
+"""
+Write a program that checks whether an integer is a palindrome. 
+For example, 121 is a palindrome, as well as 888. 678 is not a
+palindrome. Do not convert the integer into a string.
+"""
+
+def palindrome_check(n):
+    if n < 0:
+        return False
+    rev, temp = 0, n
+    while temp:
+        rev = rev * 10 + temp % 10
+        temp //= 10
+    return n == rev
+
+
+#Testing 
+print(palindrome_check(121))  
+print(palindrome_check(888))  
+print(palindrome_check(678)) 
